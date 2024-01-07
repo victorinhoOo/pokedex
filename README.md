cf41e53bf34d44762c77f4e02b9938d80ed3104a
Victor Duboz <dubozvictor@gmail.com>
2024-01-08 00:30:31 +0100

    TP 9 Fini : Modifications esthétiques

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # 2023-2024-R3-01-C2-DUBOZVICTOR
 
 http://grp-440.iq.iut21.u-bourgogne.fr/vscode/Pokedex/2023-2024-R3-01-C2-DUBOZVICTOR/index.php
-Avancée : TP 8 Fini
-          TP 9 fini à 33%
+Avancée : Pokédex fini
--- views/gabarit.php
+++ views/gabarit.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8"/>
     <link rel="stylesheet" href="public/css/main.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">         
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/1169/1169608.png" type="image/png">         
     <title>Pokédex de Victor</title>
 </head>
 
@@ -13,10 +14,10 @@
     <header>
         <nav class="menu">
             <a href="https://iutdijon.u-bourgogne.fr/www/" class="menu-item"><img src="https://iutdijon.u-bourgogne.fr/www/wp-content/uploads/2023/07/IMAG_changement_logo_rentree_2023.jpg" class="iut-link"></a>
-            <a href="index.php" class="menu-item">Accueil</a>
-            <a href="index.php?action=add-pokemon" class="menu-item">Ajouter un pokémon</a>
-            <a href="index.php?action=add-type" class="menu-item">Ajouter un type</a>
-            <a href="index.php?action=search" class="menu-item">Rechercher</a>
+            <a href="index.php" class="menu-item"><i class="fas fa-home"></i> Accueil</a>
+            <a href="index.php?action=add-pokemon" class="menu-item"><i class="fas fa-plus-circle"></i> Ajouter un pokémon</a>
+            <a href="index.php?action=add-type" class="menu-item"><i class="fas fa-plus-circle"></i> Ajouter un type</a>
+            <a href="index.php?action=search" class="search-nav"><i class="fas fa-search"></i> Rechercher</a>
         </nav>
     </header>
 
